89bce3a2f3f78037b7775683d4fae87f7d5a2079
mjaylower <35702869+mjaylower@users.noreply.github.com>
2018-09-17 15:07:31 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ public class Main{
 
     public static void main(String[] args) {
 
+        // setting the values to be sent to EmployeeContact class
+        // values are parsed by array order
         EmployeeContact employee1 = new EmployeeContact("Michael", "Lower", "42", 2);
         EmployeeContact employee2 = new EmployeeContact("Rachel", "Miller", "21", 1);
         EmployeeContact employee3 = new EmployeeContact("Mark", "Riley", "289", 3);
 
+        // calling the print method from the EmployeeContact class to be displayed here
         employee1.print();
         System.out.println("\n");
         employee2.print();
